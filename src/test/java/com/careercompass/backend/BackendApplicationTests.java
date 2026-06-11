8bc719d6997db492c986943fc3b3f835a7f7bd18
Yash Gupta <iamyashgupta55@gmail.com>
2026-06-12 01:59:45 +0530

    Initial project setup

---
+++ src/test/java/com/careercompass/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careercompass.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
