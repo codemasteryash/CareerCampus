1451e9b18090a5c0fa599a41bef554467628fda7
Yash Gupta <iamyashgupta55@gmail.com>
2026-06-18 02:05:30 +0530

    application properties

--- src/main/java/com/careercompass/backend/user/repository/UserRepository.java
+++ src/main/java/com/careercompass/backend/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.careercompass.backend.user.repository;
 
-import com.careercompass.backend.entity.User;
+import com.careercompass.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
